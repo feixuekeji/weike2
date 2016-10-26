40817ae38debc2fd445dbdcb6fb19cc4e326835b
feixuekeji <waxiongfeifei@gemail.com>
2016-10-26 11:14:54 +0800

    a

--- testmysql.php
+++ testmysql.php
@@ -3,5 +3,6 @@ $link = mysql_connect('hostname','dbuser','dbpassword');
 if (!$link) { 
 	die('Could not connect to MySQL: ' . mysql_error()); 
 } 
+echo "aaa";
 echo 'Connection OK'; mysql_close($link); 
 ?> 
